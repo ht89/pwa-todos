e6a08f2e3f9994a7b4d35bdbef2984530bd950d7
Hung Tran <hungtran891989@gmail.com>
2020-12-12 19:42:53 +0700

    install firebase, @angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,11 @@ import { HomeModule } from './home/home.module';
 import { ShellModule } from './shell/shell.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+
+import { environment } from '@env/environment';
 
 @NgModule({
   imports: [
@@ -22,6 +27,9 @@ import { AppRoutingModule } from './app-routing.module';
     HomeModule,
     AuthModule,
     AppRoutingModule, // must be imported as the last module as it contains the fallback route
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
   ],
   declarations: [AppComponent],
   providers: [],
