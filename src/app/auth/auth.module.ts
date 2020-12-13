3047057a3b2b705cd5c60816970b188aa0f6253c
Hung Tran <hungtran891989@gmail.com>
2020-12-13 16:47:07 +0700

    add needed primeng components

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './login.component';
 import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
-  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule, InputTextModule],
+  imports: [CommonModule, ReactiveFormsModule, AuthRoutingModule, InputTextModule, ButtonModule],
   declarations: [LoginComponent],
 })
 export class AuthModule {}
