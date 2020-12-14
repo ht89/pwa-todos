b31cc2b9f5558cd089d1d0f7b7a3570dcb9c2858
Hung Tran <hungtran891989@gmail.com>
2020-12-14 07:00:24 +0700

    modify page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -58,7 +58,7 @@ export class AppComponent implements OnInit, OnDestroy {
       .subscribe((event) => {
         const title = event.title;
         if (title) {
-          this.titleService.setTitle(title);
+          this.titleService.setTitle(`PWA-Todos - ${title}`);
         }
       });
   }
