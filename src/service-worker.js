04a728a91d180cfe9442994bcd27f5387d5bcf52
Hung Tran <hungtran891989@gmail.com>
2020-12-15 07:27:40 +0700

    define what needs to be cached

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,3 +1,25 @@
-self.addEventListener('fetch', (event) => {
-  console.log('Fetch request for:', event.request.url);
+const CACHE_NAME = 'pwa-todos-v1';
+const CACHED_URLS = [
+  // JS
+  '/runtime.js',
+  '/polyfills.js',
+  '/styles.js',
+  '/vendor.js',
+  '/main.js',
+  '/firebase-auth.js',
+
+  // CSS
+
+  // Fonts
+  '/source-sans-pro-v10-latin-ext_latin-regular.woff2',
+  '/primeicons.ttf',
+
+  // Images
+  '/assets/layout/images/logo-manhattan.png',
+  '/assets/ngx-rocket-logo.png',
+  '/favicon.ico',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(CACHED_URLS)));
 });
