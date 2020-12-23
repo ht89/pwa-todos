27dd8c2ae93e5fa68eae88f6ac25b52e0c9fe1df
Hung Tran <hungtran891989@gmail.com>
2020-12-23 08:05:54 +0700

    display projects

--- src/app/pages/projects/projects.module.ts
+++ src/app/pages/projects/projects.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '@shared';
 import { ProjectsRoutingModule } from './projects-routing.module';
 import { ProjectsComponent } from './projects.component';
+import { TableModule } from 'primeng/table';
 
 @NgModule({
   declarations: [ProjectsComponent],
-  imports: [CommonModule, SharedModule, ProjectsRoutingModule],
+  imports: [CommonModule, SharedModule, ProjectsRoutingModule, TableModule],
 })
 export class ProjectsModule {}
