1c121c9ad25e55d3f8052e77f083f3931b1b86e0
Hung Tran <hungtran891989@gmail.com>
2021-01-01 12:20:54 +0700

    define types for db & transaction

--- src/app/@core/indexed-db/migrations.ts
+++ src/app/@core/indexed-db/migrations.ts
@@ -5,7 +5,7 @@ import { StoreName } from '@app/@shared';
 export function migrationFactory() {
   // Define more migrations as key-value pairs (key = version)
   return {
-    1: (db, transaction) => {
+    1: (db: IDBDatabase, transaction: IDBTransaction) => {
       const store = transaction.objectStore(StoreName.Projects);
       store.createIndex('idx_id', 'id', { unique: true });
       store.createIndex('idx_status', 'status', { unique: false });
