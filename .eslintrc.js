affe5652aa321ffebf3b9313c3b70a3ef1eddb1b
Hung Tran <hungtran891989@gmail.com>
2021-01-01 18:55:10 +0700

    set endOfLine to auto

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,4 +9,8 @@ module.exports = {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
   },
+  rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    endOfLine: 'auto',
+  },
 };
